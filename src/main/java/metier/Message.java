8167f4fb3e75b7cfa2c174453878b3a44d76ad8e
Mustapha <elabridimustapha@gmail.com>
2025-06-10 22:12:56 +0200

    Ajout de Persistence et Merge request

--- src/main/java/metier/Message.java
+++ src/main/java/metier/Message.java
@@ -1,33 +1,65 @@
 package metier;
 
 import jakarta.persistence.*;
-import java.io.Serializable;
+
+import java.sql.Timestamp;
 import java.time.LocalDateTime;
 
 @Entity
 @Table(name = "Message")
-public class Message implements Serializable {
-
+public class Message {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private int id;
-
-    @Column(name = "contenu")
+    private Integer id;
+    @Column(nullable = false, length = 255)
     private String contenu;
-
-    @Column(name = "temps")
+    @Column(nullable = false)
     private LocalDateTime temps;
+    @ManyToOne
+    @JoinColumn(name = "idUtilisateurEmetteur")
+    private Utilisateur utilisateurEmetteur;
+    @ManyToOne
+    @JoinColumn(name = "idUtilisateurRecepteur")
+    private Utilisateur utilisateurRecepteur;
 
-    // Constructeur vide
     public Message() {}
+    public Message(String contenu, LocalDateTime temps) {
+        this.contenu = contenu;
+        this.temps = temps;
+    }
 
-    // Getters et setters
-    public int getId() { return id; }
-    public void setId(int id) { this.id = id; }
+    public int getId() {
+        return id;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+    public String getContenu() {
+        return contenu;
+    }
+    public void setContenu(String contenu) {
+        this.contenu = contenu;
+    }
 
-    public String getContenu() { return contenu; }
-    public void setContenu(String contenu) { this.contenu = contenu; }
-
-    public LocalDateTime getTemps() { return temps; }
-    public void setTemps(LocalDateTime temps) { this.temps = temps; }
+    public LocalDateTime getTemps() {
+        return temps;
+    }
+    public void setTemps(LocalDateTime temps) {
+        this.temps = temps;
+    }
+    public Utilisateur getUtilisateurEmetteur() {
+        return utilisateurEmetteur;
+    }
+    public void setUtilisateurEmetteur(Utilisateur utilisateurEmetteur) {
+        this.utilisateurEmetteur = utilisateurEmetteur;
+    }
+    public Utilisateur getUtilisateurRecepteur() {
+        return utilisateurRecepteur;
+    }
+    public void setUtilisateurRecepteur(Utilisateur utilisateurRecepteur) {
+        this.utilisateurRecepteur = utilisateurRecepteur;
+    }
+    public int getUtilisateurEmetteurId() {
+        return utilisateurEmetteur.getId();
+    }
 }
